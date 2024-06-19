3758f4f61319adccfec47a36b3b2c71bdd42407a
nezuo <hinckleytmicah@gmail.com>
2024-06-18 17:17:26 -0700

    Allow for multiple substrings in shouldThrow

--- src/x/shouldThrow.lua
+++ src/x/shouldThrow.lua
@@ -13,15 +13,25 @@
 	@param callback () -> ()
 	@param substring string?
 ]=]
-local function shouldThrow(callback: () -> (), substring: string?)
+local function shouldThrow(callback: () -> (), ...: string)
 	local ok, err = pcall(callback)
 
 	err = tostring(err)
 
 	if ok then
 		error("expected callback to throw, but it didn't throw", 2)
-	elseif substring ~= nil and string.find(err, substring, 1, true) == nil then
-		error(`expected callback to throw an error containing {substring}, but it threw: {err}`, 2)
+	end
+
+	local excludedSubstring = nil
+	for _, substring in { ... } do
+		if string.find(err, substring, 1, true) == nil then
+			excludedSubstring = substring
+			break
+		end
+	end
+
+	if excludedSubstring ~= nil then
+		error(`expected callback to throw an error containing {excludedSubstring}, but it threw: {err}`, 2)
 	end
 end
 
